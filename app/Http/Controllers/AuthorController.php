7b465a56a96b35ed165b4db6ef21e4fac5945246
Biswajit Biswas <jfbiswajit@gmail.com>
2020-10-01 15:16:09 +0600

    Add: all possible action template added

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthorController extends Controller
+{
+
+  public function index()
+  {
+  }
+
+  public function store()
+  {
+  }
+
+  public function show()
+  {
+  }
+
+  public function update()
+  {
+  }
+
+  public function destroy()
+  {
+  }
+}
